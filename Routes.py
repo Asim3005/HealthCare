3d902fc84c03dda0c16ccc058abb0d6241f9b257
Star-Mech <HammadStarMech@gmail.com>
2022-09-09 20:15:56 +0500

    Removing redundant

--- Routes.py
+++ Routes.py
@@ -272,37 +272,6 @@ def delete_patient2():
     else:
         flash("patient delete failed . Please try again", "danger")
         return redirect(url_for('delete_patient'))
-
-
-# ==================================================================================
-#                       Search for existing patient using Patient ID
-# ==================================================================================
-
-
-@app.route("/SearchPatient", methods=["GET", "POST"])
-def search_patient():
-
-    # Check that an authorised user only can access this functionality
-    # if check_session() != 'registration_desk_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-
-    form = Patient_delete()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            global pid
-            pid = int(form.patient_id.data)
-            # Query for patient_details
-            patient = Patient_details.query.filter(
-                Patient_details.id == int(form.patient_id.data))
-            for patient_1 in patient:
-                if patient_1:
-                    flash("patient found", "success")
-                    return render_template("search_patient.html", title="Search patient", patient=patient, form=form)
-            flash("patient not found", "danger")
-    return render_template("search_patient.html", title="Search Patient", form=form)
-
-
 # ==================================================================================
 #                    Update the detains of an existing patient
 # ==================================================================================
@@ -535,15 +504,6 @@ def update():
     return redirect(url_for('get_patient'))
 
 
-# function to retrieve patient medicines
-def med_patient(patient):
-    mid = patient.id
-    if Patient_Medicine.query.filter(Patient_Medicine.patient_id == mid).first() == None:
-        return None
-    else:
-        x = Patient_Medicine.query.join(Medicine, Patient_Medicine.medicine_id == Medicine.id).filter(
-            Patient_Medicine.patient_id == mid)
-        return x
 
 
 # ==================================================================================
@@ -551,149 +511,9 @@ def med_patient(patient):
 # ==================================================================================
 
 
-@app.route("/GetPatientDetails/Diagnostics", methods=["GET", "POST"])
-def patient_diagnosis():
-
-    # Check that an authorised user only can access this functionality
-    # if check_session() != 'diagnostic_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-
-    form = Patient_delete()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            global pid
-            global add_test
-            pid = int(form.patient_id.data)
-            patient = Patient_details.query.filter(
-                Patient_details.id == int(form.patient_id.data))
-            for patient_1 in patient:
-                if patient_1:
-                    add_test = None
-                    flash("patient found", "success")
-                    # Query for patient_diagnostics
-                    if Patient_test.query.filter(Patient_test.patient_id == patient_1.id).first() == None:
-                        return render_template("get_patient_diagnosis.html", title="Search patient", patient=patient, pid=pid)
-                    else:
-                        x = Patient_test.query.join(Diagnosis, Patient_test.test_id == Diagnosis.id).filter(
-                            Patient_test.patient_id == patient_1.id)
-                        return render_template("get_patient_diagnosis.html", title="Search patient", patient=patient, pid=pid, tests=x)
-            flash("patient not found", "danger")
-    return render_template("get_patient_diagnosis.html", title="Get Patient Diagnostics", form=form)
-
-
-@app.route("/Diagnostics", methods=["GET", "POST"])
-def diagnostics():
-
-    # Check that an authorised user only can access this functionality
-    # if check_session() != 'diagnostic_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-    global pid
-    global add_test
-    form = add_diagnosis()
-    if form.validate_on_submit():
-        testname = form.diagnosis.data
-        # Query for Diagnostics
-        test = Diagnosis.query.filter(Diagnosis.test_name == testname).first()
-        if add_test == None:
-            add_test = {}
-            add_test[testname] = {'name': testname, 'amount': test.test_amount}
-        else:
-            add_test[testname] = {'name': testname, 'amount': test.test_amount}
-        flash("medicine added", "success")
-        return render_template("diagnostics.html", pid=pid, title="Conduct Diagnostics", form=form, tests=add_test)
-    return render_template("diagnostics.html", pid=pid, title="Conduct Diagnostics", form=form, tests=add_test)
-
-
-@app.route('/updatetest', methods=['GET', 'POST'])
-def update_test():
-    # if check_session() != 'diagnostic_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-    global pid
-    global add_test
-    for i in add_test:
-        name = add_test[i]['name']
-        # Query for diagnostics
-        test = Diagnosis.query.filter(Diagnosis.test_name == name).first()
-        tid = test.id
-        # Add the diagnostic to db
-        db.session.add(Patient_test(patient_id=pid, test_id=tid))
-        db.session.commit()
-    add_test = None
-    flash("Successfully updated", "success")
-    return redirect(url_for('patient_diagnosis'))
-
-
 # ==================================================================================
 #                                   Patient Billing
 # ==================================================================================
-
-@app.route('/FinalBilling', methods=["GET", "POST"])
-def billing():
-
-    # Check that an authorised user only can access this functionality
-    # if check_session() != 'registration_desk_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-    mbill = 0
-    tbill = 0
-    form = Patient_delete()
-    if request.method == 'POST':
-        mbill = 0
-        tbill = 0
-        if form.validate_on_submit():
-            # Query for patient details
-            patient = Patient_details.query.filter(
-                Patient_details.id == int(form.patient_id.data))
-            for patient_1 in patient:
-                if patient_1:
-                    flash("Patient found", "success")
-                    mbill = 0
-                    tbill = 0
-                    medicine = med_patient(patient_1)
-                    if medicine:
-                        for m in medicine:
-                            mbill = mbill + \
-                                (m.medicine.medicine_amount*m.medicine_quantity)
-                    # Query for patient diagnostics
-                    test = Patient_test.query.join(Diagnosis, Patient_test.test_id == Diagnosis.id).filter(
-                        Patient_test.patient_id == patient_1.id)
-                    if test:
-                        for t in test:
-                            tbill = tbill+t.diagnosis.test_amount
-                    # Calculate the number of days since the admission
-                    days = date.today() - patient[0].admission_date
-                    # Calculate the room charges
-                    if patient[0].bed_type.lower() == 'general ward':
-                        charges = 2000
-                    elif patient[0].bed_type.lower() == 'semi sharing':
-                        charges = 4000
-                    elif patient[0].bed_type.lower() == 'single room':
-                        charges = 8000
-                    return render_template('billing.html', patient=patient, medicine=medicine, tests=test, mbill=mbill, tbill=tbill, days=days, charges=charges, total=str(days * charges).replace("days, 0:00:00", ""), pid=patient[0].id)
-            flash("Patient not found!", "danger")
-    return render_template('billing.html', form=form, tbill=tbill, mbill=mbill, total=0)
-
-
-@app.route('/Discharge', methods=["POST"])
-def discharge():
-    # if check_session() != 'registration_desk_executive':
-    #     flash('You are not authorised to access that! Please login with proper credentials.', 'danger')
-    #     return redirect(url_for('main'))
-    patient = Patient_details.query.filter_by(
-        id=request.form.get('pid')).first()
-    if patient:
-        # Check if the patient is already discharged
-        if patient.status == 'Discharged':
-            flash("Patient already Discharged!", "danger")
-        else:
-            # Discharge the patient
-            patient.status = 'Discharged'
-            db.session.commit()
-            flash("Successfully Discharged the patient!", "success")
-    return redirect(url_for('billing'))
 
 # ==================================================================================
 #                                 Delete the user Session
